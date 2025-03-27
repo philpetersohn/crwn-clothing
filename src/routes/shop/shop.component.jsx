0933ca8bd486386f88fbf8d137c857bfc87be7c4
philpetersohn <petersohn.phil@gmail.com>
2025-03-27 16:44:56 -0700

    Add category management and routing to Shop component
    - Updated Shop component to use nested routes for category previews and individual category views.
    - Introduced CategoryPreview component to display a preview of products for each category.
    - Added Category component to show products for a specific category based on URL parameters.
    - Created corresponding styles for category previews and individual category displays.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,16 @@
-import { useContext } from 'react'
-import { CategoriesContext } from '../../context/categories.context'
-import ProductCard from '../../components/product-card/product-card.component'
+import { Routes, Route } from 'react-router-dom'
+
+import CategoriesPreview from '../categories-preview/categories-preview.component'
+import Category from '../category/category.component'
 
 import './shop.styles.scss'
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext)
-
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <div key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      ))}
-    </>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   )
 }
 
